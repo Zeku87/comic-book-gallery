bc3a33156f2e0a419e03d5444fff08f137fea6e8
J Ezequiel Gallardo <udev87@outlook.com>
2017-12-28 18:07:31 +0100

    ViewBag added to Detail View

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,17 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Details()
         {
+            ViewBag.seriesTitle = "The Amazing Spider-Man";
+            ViewBag.issueNumber = 700;
+            ViewBag.description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.artists = new String[]
+            {
+        "Script: Dan Slott",
+        "Pencils: Humberto Ramos",
+        "Inks: Victor Olazaba",
+        "Colors: Edgar Delgado",
+        "Letters: Chris Eliopoulos"
+            };
             return View();
         }
     }
