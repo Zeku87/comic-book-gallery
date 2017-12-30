338cb8c605bef70b762b190ee7ab23fe0b89704d
J Ezequiel Gallardo <udev87@outlook.com>
2017-12-30 15:04:18 +0100

    Strongly type view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ComicBookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace ComicBookGallery.Controllers
 {
@@ -10,18 +12,22 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Details()
         {
-            ViewBag.seriesTitle = "The Amazing Spider-Man";
-            ViewBag.issueNumber = 700;
-            ViewBag.description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.artists = new String[]
+            ComicBook comicSample = new ComicBook()
             {
-        "Script: Dan Slott",
-        "Pencils: Humberto Ramos",
-        "Inks: Victor Olazaba",
-        "Colors: Edgar Delgado",
-        "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p> Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott" , Role = "Script"},
+                    new Artist() { Name = "Humberto Ramos" , Role = "Pencils"},
+                    new Artist() { Name = "Victor Olazaba" , Role = "Inks"},
+                    new Artist() { Name = "Edgar Delgado" , Role = "Colors"},
+                    new Artist() { Name = "Chris Eliopoulos" , Role = "Letters"},
+                }
             };
-            return View();
+
+            return View(comicSample);
         }
     }
 }
